fa290e323ab9c196f6423cd266db719a330da6e0
Millat Hossain <millathossain115@gmail.com>
2023-08-07 22:02:47 +0600

    Showing Topic to index Dynamically

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
+<?php require "../forum/includes/fydpHeader.php"; ?>
+<?php require "../forum/config/config.php"; ?>
 
-<?php require "includes/fydpHeader.php"; ?>
+<?php 
 
+	$topics = $conn->query("SELECT topics.id AS id, topics.title AS title, topics.category AS category,
+	topics.user_name AS user_name,
+	topics.user_image AS user_image,
+	topics.created_at AS created_at,
+	COUNT(replies.topic_id) AS count_replies FROM topics JOIN replies ON
+	topics.id = replies.topic_id GROUP BY(replies.topic_id)");
+
+	$topics->execute();
+	$a11Topics = $topics->fetchAll(PDO::FETCH_OBJ);
+
+?>
 
 <main>
     <section class="container mt-4">
@@ -8,12 +21,6 @@
     </section>
 
 </main>
- <!-- <div class="container bg-gradient">
-		<div class="row bg-light-subtle">
-			<div class=" col-lg-8  bg-danger" id="">
-				
-        		<div class="main-col bg-black"> -->
-
 
 
 <div class="container">
@@ -31,147 +38,46 @@
 
 						<hr class="fw-bolder text-dark mx-3">
 
-							<!--================== Topic 0 ==================-->
+
+
+						<!--================== Topic 0 ==================-->
 						
 						<ul class="list-unstyled" id="topics">
-							
+							<?php foreach($a11Topics as $topic) : ?>
+
 							<li class="topic">
 							<div class="row  h-25  mx-3 rounded-3 border my-2">
 
 							<div class="col-md-2 ">
-								<!-- <img class="" src="img/photo_2023-07-27_02-45-20.jpg" alt="Bootstrap" width="45" height="45" /> -->
-								<img class="avatar pull-left h-75 w-75 mx-3 mt-2 rounded-circle p-2" src="img/millat.jpg" />
+								
+								<img class="avatar pull-left h-75 w-75 mx-3 mt-2 rounded-circle p-2" src="../forum/img/<?php echo $topic->user_image ; ?>" />
 							</div>
 
 							<div class="col-md-10 ">
 								<div class="">
-									<h5 class="mt-3 mb-0"><a class="text-decoration-none" href="topic.html">How reliable is the Toyota Corolla?</a></h5>
+									<h5 class="mt-3 mb-0"><a class="text-decoration-none" href="topic.html"><?php echo $topic->title; ?></a></h5>
 
 									<div class="topic-info">
-										<a class="text-decoration-none" href="category.html">Toyota</a> >> <a class="text-decoration-none" href="profile.html">Millat Hossain</a> >> Posted on: June 12, 2014 
-										<span class="badge rounded-pill text-bg-primary float-end me-4">3</span>
+										<a class="text-decoration-none" href="category.html"><?php echo $topic->category; ?></a> >> <a class="text-decoration-none" href="profile.html"><?php echo $topic->user_name ; ?></a> >> Posted on: <?php echo $topic->created_at ; ?> 
+										<span class="badge rounded-pill text-bg-primary float-end me-4"><?php echo $topic->count_replies ; ?></span>
 									</div>
 
 								</div>
 							</div>
 						</div>
-            				<!-- <hr class="fw-bolder text-dark mx-4"> -->
+            				<hr class="fw-bolder text-white mx-4">
 							</li>
+							<?php endforeach;?>
 							
-							<!--================== Topic 1 ==================-->
-
-						<ul class="list-unstyled" id="topics">
-
-							<li class="topic">
-							<div class="row  h-25 border mx-3 rounded-3 my-2">
-
-							<div class="col-md-2 ">
-								<img class="avatar pull-left h-75 w-75 mx-3 mt-2 p-2 rounded-circle" src="img/gravatar.png" />
-							</div>
-
-							<div class="col-md-10 ">
-								<div class="">
-									<h5 class="mt-3 mb-0"><a class="text-decoration-none" href="topic.html">How reliable is the Toyota Corolla?</a></h5>
-
-									<div class="topic-info">
-										<a class="text-decoration-none" href="category.html">Toyota</a> >> <a class="text-decoration-none" href="profile.html">Millat Hossain</a> >> Posted on: June 12, 2014 
-										<span class="badge rounded-pill text-bg-primary float-end me-4">3</span>
-									</div>
-
-								</div>
-							</div>
-							</div>
-            				<!-- <hr class="fw-bolder text-dark mx-4"> -->
-							</li>
-							
-							<!--================== Topic 2 ==================-->
-
-						<ul class="list-unstyled" id="topics">
-							
-							<li class="topic">
-							<div class="row  h-25 border mx-3 rounded-3 my-2">
-
-							<div class="col-md-2 ">
-								<img class="avatar pull-left h-75 w-75 mx-3 mt-2 p-2" src="img/gravatar.png" />
-							</div>
-
-							<div class="col-md-10 ">
-								<div class="">
-									<h5 class="mt-3 mb-0"><a class="text-decoration-none" href="topic.html">How reliable is the Toyota Corolla?</a></h5>
-
-									<div class="topic-info">
-										<a class="text-decoration-none" href="category.html">Toyota</a> >> <a class="text-decoration-none" href="profile.html">Millat Hossain</a> >> Posted on: June 12, 2014 
-										<span class="badge rounded-pill text-bg-primary float-end me-4">3</span>
-									</div>
-
-								</div>
-							</div>
-						</div>
-            <!-- <hr class="fw-bolder text-dark mx-4"> -->
-							</li>
-							
-							<!--================== Topic 3 ==================-->
-
-						<ul class="list-unstyled" id="topics">
-							
-							<li class="topic">
-							<div class="row  h-25 border mx-3 rounded-3 my-2">
-
-							<div class="col-md-2 ">
-								<img class="avatar pull-left h-75 w-75 mx-3 mt-2 p-2" src="img/gravatar.png" />
-							</div>
-
-							<div class="col-md-10 ">
-								<div class="">
-									<h5 class="mt-3 mb-0"><a class="text-decoration-none" href="topic.html">How reliable is the Toyota Corolla?</a></h5>
-
-									<div class="topic-info">
-										<a class="text-decoration-none" href="category.html">Toyota</a> >> <a class="text-decoration-none" href="profile.html">Millat Hossain</a> >> Posted on: June 12, 2014 
-										<span class="badge rounded-pill text-bg-primary float-end me-4">3</span>
-									</div>
-
-								</div>
-							</div>
-						</div>
-            <!-- <hr class="fw-bolder text-dark mx-4"> -->
-							</li>
-							
+							<!--================== Topic 1 ==================-->						
+							<!--================== Topic 2 ==================-->						
+							<!--================== Topic 3 ==================-->				
 							<!--================== Topic 4 ==================-->
-
-						<ul class="list-unstyled" id="topics">
-							
-							<li class="topic">
-							<div class="row  h-25 border mx-3 rounded-3 my-2">
-
-							<div class="col-md-2 ">
-								<img class="avatar pull-left h-75 w-75 mx-3 mt-2 p-2" src="img/gravatar.png" />
-							</div>
-
-							<div class="col-md-10 ">
-								<div class="">
-									<h5 class="mt-3 mb-0"><a class="text-decoration-none" href="topic.html">How reliable is the Toyota Corolla?</a></h5>
-
-									<div class="topic-info">
-										<a class="text-decoration-none" href="category.html">Toyota</a> >> <a class="text-decoration-none" href="profile.html">Millat Hossain</a> >> Posted on: June 12, 2014 
-										<span class="badge rounded-pill text-bg-primary float-end me-4">3</span>
-									</div>
-
-								</div>
-							</div>
-						</div>
-            			<hr class="fw-bolder text-white mx-4">
-							</li>
-					
+			
 						</ul>
 						
 					</div>
 				</div>
 			</div>
 
-			<!-- Sidebar counted as footer1 -->
-			<!-- <div class="col-md-12 col-lg-4">
-			<div class="sidebar">		 -->
-
-
-
-                <?php require "includes/fydpFooter.php"; ?>
+<?php require "includes/fydpFooter.php"; ?>
