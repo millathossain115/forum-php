3df382ad8b0a239b897b294689e7f5ace6231845
Millat Hossain <millathossain115@gmail.com>
2023-08-19 01:10:06 +0600

    Started with Admin DashBoard

---
+++ admin-panel/index.php
@@ -0,0 +1,53 @@
+<?php require "../admin-panel/layouts/header.php"; ?>
+<?php //require "../config/config.php"; ?>
+
+
+    <div class="container-fluid  mx-auto ms-lg-5">
+            
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+
+        <div class="col col-lg-3 col-md-6">
+          <div class="card shadow mb-3 h-100">
+            <div class="card-body">
+              <h5 class="card-title">Topics</h5>
+              <!-- <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6> -->
+              <p class="card-text">Total Topics: 8</p>
+             
+            </div>
+          </div>
+        </div>
+
+        <div class="col col-lg-3 col-md-6">
+          <div class="card shadow mb-3 h-100">
+            <div class="card-body">
+              <h5 class="card-title">Categories</h5>
+              
+              <p class="card-text">Total Categories: 4</p>
+              
+            </div>
+          </div>
+        </div>
+
+        <div class="col col-lg-3 col-md-6">
+          <div class="card shadow mb-3 h-100">
+            <div class="card-body">
+              <h5 class="card-title">Admins</h5>
+              
+              <p class="card-text">Total Admins: 3</p>
+              
+            </div>
+          </div>
+        </div>
+        <div class="col col-lg-3 col-md-6">
+          <div class="card shadow mb-3 h-100">
+            <div class="card-body">
+              <h5 class="card-title">Replies</h5>
+              
+              <p class="card-text">Total Replies: 3</p>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+
+<?php require "../admin-panel//layouts/footer.php"; ?>
---
+++ admin-panel/layouts/header.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Admin AutowhiZ</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="styles/style.css" rel="stylesheet">
+  </head>
+  <body>
+    
+
+<section class="container" id="wrapper">
+
+    <nav class="navbar  fixed-top navbar-expand-lg  navbar-dark bg-dark shadow">
+      <div class="container-fluid">
+     
+        <a class="navbar-brand" href="#">
+          <img src="../admin-logo.png" alt="" width="29" height="29" class="ms-4" href="">
+          <span class="fw-bold" style="color: #FF1700;">Auto</span><span class="" style="color: #F7EA00;">whiZ</span></a>
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav side-nav bg-dark text-white" >
+
+          <li class="nav-item">
+            <a class="nav-link text-light" style="margin-left: 20px;" href="index.html">Home
+              <span class="visually-hidden">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light" href="admins/admins.html" style="margin-left: 20px;">Admins</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light" href="categories-admins/show-categories.html" style="margin-left: 20px;">Categories</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light" href="topics-admins/show-topics.html" style="margin-left: 20px;">Topics</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light" href="replies-admins/show-replies.html" style="margin-left: 20px;">Replies</a>
+          </li>
+        </ul>
+
+
+<!-- Side Nav Ended -->
+
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item">
+            <a class="nav-link text-light" href="index.html">Home
+              <span class="visually-hidden">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light" href="admins/login-admins.html">login
+              <span class="visually-hidden">(current)</span>
+            </a>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link text-light dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"  aria-expanded="false">
+              username
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item text-light p-0 px-2" href="#">Logout</a>
+              
+          </li>
+                          
+          
+        </ul>
+      </div>
+    </div>
+    </nav>
+
+    <!-- Navbar Ended -->
